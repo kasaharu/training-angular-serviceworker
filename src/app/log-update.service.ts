63804b76ee50f4d458daa9bc9d1c9a27b4d48e07
Wataru.Kasahara <kasahara1019@gmail.com>
2018-02-06 23:53:13 +0900

    SwUpdate の available を実装

--- src/app/log-update.service.ts
+++ src/app/log-update.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Injectable()
 export class LogUpdateService {
 
-  constructor() { }
+  constructor(updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      console.log('current version is ', event.current);
+      console.log('available version is', event.available);
+    });
+  }
 
 }
